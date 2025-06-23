10ca230268e51b5dce3a61ae346c7a39525e720c
Sanjyot Agureddy <sanjyot.agureddy4@gmail.com>
2025-06-23 22:09:14 +0530

    MSX-59: Moving logging-service to cross-cutting-service

---
+++ test/MeraStore.Services.Cross.Cutting.Api.Architecture.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MeraStore.Services.Cross.Cutting.Api.Architecture.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
